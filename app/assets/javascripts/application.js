df3f9755180d0f960907567f5eba0badf3ec61cc
Sergio Tapia <sergiotapia@outlook.com>
2014-06-19 16:07:32 -0400

    Attach DropzoneJS to the compile asset pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,21 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require dropzone
 //= require_tree .
+
+Dropzone.options.photoDropzone = {
+  paramName: "image",
+  maxFilesize: 2,
+  addRemoveLinks: true,
+  init: function() {
+    return this.on('removedfile', function(file) {
+      if (file.xhr) {
+        return $.ajax({
+          url: "" + ($("#picture-dropzone").attr("action")) + "/" + (JSON.parse(file.xhr.response).id),
+          type: 'DELETE'
+        });
+      }
+    });
+  }
+};
