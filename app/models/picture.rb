d4cd2e7db44c5d1a530ea32973ae66fb3446e267
Sergio Tapia <sergiotapia@outlook.com>
2014-06-19 14:00:06 -0400

    Integrate uploader to the Picture model and define database attribute to save uploaded file.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,5 @@
 class Picture < ActiveRecord::Base
   belongs_to :product
+
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619175411) do
+ActiveRecord::Schema.define(version: 20140619175727) do
 
   create_table "pictures", force: true do |t|
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   add_index "pictures", ["product_id"], name: "index_pictures_on_product_id"
